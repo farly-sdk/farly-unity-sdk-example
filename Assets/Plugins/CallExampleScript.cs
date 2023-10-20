ff59f0a52210bf3b905bce64b4d518cb4ba3d419
Philippe Auriach <p.auriach@gmail.com>
2023-10-20 17:19:10 +0200

    Initial commit

---
+++ Assets/Plugins/CallExampleScript.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FarlySDK;
+
+public class CallExampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("CallingExample - started");
+        //TODO: replace with your own apiKey and publisherId
+        Farly.Configure("apiKey", "publisherId");
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public void showOfferwallInBrowser()
+    {
+        var offerwallRequest = new OfferWallRequest
+        {
+            userId = "your_userId",
+            userGender = "m",
+            userAge = 25,
+            zipCode = "75017",
+            countryCode = "FR",
+            userSignupDateTime = new DateTime(2019, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+            callbackParameters = new string[] { "cb1", "cb2" },
+        };
+        Farly.showOfferwallInBrowser(offerwallRequest);
+    }
+
+    public void showOfferwallInWebview()
+    {
+        Farly.showOfferwallInWebview(new OfferWallRequest { userId = "your_userId" });
+    }
+
+    public void listOffers()
+    {
+        Farly.getOfferwall(new OfferWallRequest { userId = "your_userId" }, (offers) =>
+            {
+                // display them in your own UI ?
+                Debug.Log($"We have {offers.Length} offers to display");
+            });
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Introduction
+
+This repository is used to test and demonstrate how our Farly Unity SDK works. 
+
+# Package
+
+The package and documentation is available here at https://github.com/farly-sdk/farly-unity-sdk
+
+# Example
+
+The example code is located at `Assets/Plugins/CallExampleScript.cs`
